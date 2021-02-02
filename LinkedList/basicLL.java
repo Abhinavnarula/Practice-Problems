f26f90b5649d7d1f1c0e1a30801e285a3f88f788
abhinav narula <abhinavnarula3@gmail.com>
2021-02-02 18:43:20 +0530

    made changes in basic file

--- LinkedList/basicLL.java
+++ LinkedList/basicLL.java
@@ -2,7 +2,7 @@ class LinkedList {
 
     Node head; // head of the linkedlist.
 
-    class Node {
+    static class Node { // class is made static so that main function can access.
 
         int data; // data inside current node.
         Node next; // pointer used for pointing to next node.
@@ -11,4 +11,23 @@ class LinkedList {
             this.data = d;
         }
     }
+
+    void printfunction() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " > ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+        llist.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        llist.head.next = second;
+        second.next = third;
+        llist.printfunction();
+    }
+
 }
