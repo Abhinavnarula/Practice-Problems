4420f29be1bd67760d58015eefd992adcb256703
abhinav narula <abhinavnarula3@gmail.com>
2021-04-11 01:50:53 +0530

    changes

--- Binary Tree/bst.java
+++ Binary Tree/bst.java
@@ -28,16 +28,7 @@ public class bst {
 
     }
 
-    static int height(Node root) {
 
-        if (root == null)
-            return 0;
-
-        int l = height(root.left);
-        int r = height(root.right);
-
-        return Math.max(l, r) + 1;
-    }
 
     public static void main(String[] args) {
         // bst tree = new bst(1);
@@ -46,8 +37,6 @@ public class bst {
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-
-        System.out.println(height(root));
 
         inorder(root);
     }
