6dde7cc2d8de137b26ef126db888ef86b662eb0a
abhinav narula <abhinavnarula3@gmail.com>
2021-04-11 12:03:37 +0530

    some changes

--- Binary Tree/bst.java
+++ Binary Tree/bst.java
@@ -1,21 +1,17 @@
-class Node {
-    int data;
-    Node left;
-    Node right;
+class bst {
 
-    Node(int data) {
-        this.data = data;
-        this.left = this.right = null;
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
     }
-}
-
-public class bst {
 
     Node root;
-
-    bst(int data) {
-        root = new Node(data);
-    }
 
     static void inorder(Node root) {
 
