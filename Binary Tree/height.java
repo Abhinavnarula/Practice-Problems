58ff270e91d00358061f1e87b104444706bbae9d
abhinav narula <abhinavnarula3@gmail.com>
2021-04-11 12:08:11 +0530

    height of binary tree added

--- Binary Tree/height.java
+++ Binary Tree/height.java
@@ -1,3 +1,24 @@
-public class height extends bst {
-    
+class Height extends bst {
+    static int height(Node root){
+
+        if (root == null)
+            return 0;
+        
+            int l = height(root.left);
+            int r = height(root.right);
+
+            return Math.max(l, r) + 1;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println(height(root));
+
+        inorder(root);
+    }
 }
