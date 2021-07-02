d10fe188cf9722208d75fe834d2d3cd35cccb487
Abhinavnarula <abhinavnarula3@gmail.com>
2021-07-02 12:31:40 +0530

    Housing updated and working

--- Sliding Window/housing.java
+++ Sliding Window/housing.java
@@ -4,43 +4,38 @@ public class housing {
 
     static ArrayList<ArrayList<Integer>> house(int arr[], int n, int target) {
 
-        System.out.println(arr[0]);
+        // System.out.println(arr[0]);
         for (int i = 1; i < n; i++) {
             arr[i] += arr[i - 1];
-            System.out.println(arr[i]);
+            // System.out.println(arr[i]);
         }
 
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
 
         int start = 0, end = 0;
-        while ((start < end) || (end < n - 1) || (start < n - 1)) {
-            // System.out.println(">>" + end + " " + start);
+        while (start <= end && end < n) {
             if (arr[end] - arr[start] == target) {
-                ArrayList<Integer> temp = new ArrayList<>();
-                // System.out.println(end + " " + start);
-                temp.add(start);
+                temp.add(start + 1);
                 temp.add(end);
-                res.add(temp);
+                res.add(new ArrayList<>(temp));
                 temp.clear();
                 start++;
-            } 
-            else if (arr[end] - arr[start] > target)
+            } else if (arr[end] - arr[start] > target) {
                 start++;
-            else
+            } else {
                 end++;
+            }
         }
-
-        return null;
+        return res;
 
     }
 
     public static void main(String[] args) {
         int arr[] = { 1, 3, 2, 1, 4, 1, 3, 2, 1, 1, 2 };
         int target = 8;
-        ArrayList<ArrayList<Integer>> house = house(arr, arr.length, target);
+        ArrayList<ArrayList<Integer>> home = house(arr, arr.length, target);
+        System.out.println(home);
 
-        for (ArrayList<Integer> h : house) {
-            System.out.println(h);
-        }
     }
 }
